8038539295a2385de0b25f9887a2a070dd5c14b9
Braveram <lenchomengistu100@gmail.com>
2024-09-08 09:58:46 +0300

    ntn

--- src/States/TgContext.jsx
+++ src/States/TgContext.jsx
@@ -8,15 +8,15 @@ const TgContext = createContext();
 export function TgProvider({children}){
   const [state, dispatch] = useReducer(tgReducer, {
     isInitialized: false,
-    TG: new TelegramWebAppWrapper(window.Telegram?.WebApp || "")
+    TG: window.Telegram?.WebApp || ""
   })
   
   useEffect(()=>{
-    const tg = state.TG;
-    alert(JSON.stringify(state.TG.initData))
+    const TGD = state.TG;
     const initialize = ()=>{
-      if(tg.initData && tg.initDataUnsafe && tg.initDataUnsafe.user){  
+      if(TGD.initData && TGD.initDataUnsafe && TGD.initDataUnsafe.user){  
         dispatch({type: "SET_IS_INITIALIZED"})
+        const tg = new TelegramWebAppWrapper(TGD)
         tg.expand()
       } else{
         dispatch({type: "UNSET_IS_INITIALIZED"})
