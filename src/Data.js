028e16affa9cfbf428a7a8498d1516c05a6a4a6f
Braveram <lenchomengistu100@gmail.com>
2024-09-08 16:59:45 +0300

    Update photos

--- src/Data.js
+++ src/Data.js
@@ -4,34 +4,34 @@ export const data = [
     productName: "Laptop",
     price: 899.99,
     quantity: 0, 
-    image: "./apple-1282241_1280.jpg"
+    image: "/apple-1282241_1280.jpg"
   },
   {
     id: 2,
     productName: "Iphone 15",
     price: 799.99,
     quantity: 0,
-    image: "./apple-1841553_1280.jpg"
+    image: "/apple-1841553_1280.jpg"
   },
   {
     id: 3,
     productName: "Bicycle",
     price: 399.99,
     quantity: 0,
-    image: "./amsterdam-6744567_1280.jpg"
+    image: "/amsterdam-6744567_1280.jpg"
   },
   {
     id: 4,
     productName: "TV",
     price: 699.99,
     quantity: 0,
-    image: "./tv-flat.jpg"
+    image: "/tv-flat.jpg"
   },
   {
     id: 5,
     productName: "Tablet",
     price: 399.99,
     quantity: 0,
-    image: "./tablet-791051_1280.jpg"
+    image: "/tablet-791051_1280.jpg"
   },
 ];
