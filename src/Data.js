f222986a40a04f480b0d480ba54b7ec65ceee2cd
Braveram <lenchomengistu100@gmail.com>
2024-09-08 16:30:52 +0300

    Update photos

--- src/Data.js
+++ src/Data.js
@@ -4,34 +4,34 @@ export const data = [
     productName: "Laptop",
     price: 899.99,
     quantity: 0, 
-    image: "./laptop.jpg"
+    image: "https://unsplash.com/photos/macbook-pro-on-top-of-brown-table-1SAnrIxw5OY"
   },
   {
     id: 2,
     productName: "Iphone 15",
     price: 799.99,
     quantity: 0,
-    image: "./mobile.jpg"
+    image: "https://unsplash.com/photos/a-cell-phone-sitting-on-top-of-a-white-table-jM_iv3a_zeg"
   },
   {
     id: 3,
     productName: "Bicycle",
     price: 399.99,
     quantity: 0,
-    image: "./bicycle.jpg"
+    image: "https://unsplash.com/photos/white-road-bike-leaning-on-teal-wooden-wall-during-daytime-igKjieyjcko"
   },
   {
     id: 4,
     productName: "TV",
     price: 699.99,
     quantity: 0,
-    image: "./tv.jpg"
+    image: "https://unsplash.com/photos/a-flat-screen-tv-sitting-on-top-of-a-white-entertainment-center-uXQdBfSTBow"
   },
   {
     id: 5,
     productName: "Tablet",
     price: 399.99,
     quantity: 0,
-    image: "./tablet.jpg"
+    image: "https://unsplash.com/photos/a-pair-of-mirrors-are-hanging-on-a-wall-CX35_722cx0"
   },
 ];
