af5a418bbd53b2a3d50663bd0134563e3bd977a1
Braveram <lenchomengistu100@gmail.com>
2024-09-09 10:03:12 +0300

    fixed modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { ImSpinner } from "react-icons/im";
 import { AppProvider } from "./States/Context";
 import { Navbar } from "./Components/Navbar";
 import { Products } from "./Components/Products";
@@ -13,11 +12,13 @@ export const App = memo(()=>{
   useEffect(()=>{
      setTimeout(()=>{
        setLoading(false)
-     }, 1000)
+     }, 3000)
   }, [])
   if(loading){
     return <div className="flex items-center justify-center h-screen">
-      <h1 className="text-7xl animate-spin text-white"><ImSpinner/></h1>
+      <h1 className="animate-spin text-white">
+        <svg xmlns="http://www.w3.org/2000/svg" height="88px" viewBox="0 -960 960 960" width="88px" fill="#e8eaed"><path d="M196-331q-20-36-28-72.5t-8-74.5q0-131 94.5-225.5T480-798h43l-80-80 39-39 149 149-149 149-40-40 79-79h-41q-107 0-183.5 76.5T220-478q0 29 5.5 55t13.5 49l-43 43ZM476-40 327-189l149-149 39 39-80 80h45q107 0 183.5-76.5T740-479q0-29-5-55t-15-49l43-43q20 36 28.5 72.5T800-479q0 131-94.5 225.5T480-159h-45l80 80-39 39Z"/></svg>
+      </h1>
     </div>
   }
   if(!state.isInitialized){
--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -8,7 +8,7 @@ export function Modal({ title, children, isOpen, isFlex, isSmall }) {
   return (
     isOpen && 
     <div className="fixed inset-0 bg-black w-[100%] h-screen mx-auto block overflow-hidden z-[1000]">
-      <div className={isSmall ? "fixed right-0 inset-0 left-0 top-[15rem] bottom-0 bg-black border-[1px] border-white z-10 p-3 rounded text-center h-[150px] w-[300px] mx-auto" : "fixed right-0 left-0 inset-0 top-[15rem] z-10 bottom-0 bg-black border-[1px] border-white p-3 rounded text-center h-[180px] w-[300px] mx-auto"}>
+      <div className={isSmall ? "fixed right-0 left-0 top-[15rem] bottom-0 bg-black border-[1px] border-white z-10 p-3 rounded text-center h-[150px] w-[300px] mx-auto" : "fixed right-0 left-0 inset-0 top-[15rem] z-10 bottom-0 bg-black border-[1px] border-white p-3 rounded text-center h-[180px] w-[300px] mx-auto"}>
         <h1 className="font-extrabold mt-5">{title}</h1>
         {isFlex ? (
           <div className="flex items-center justify-center mt-2">
